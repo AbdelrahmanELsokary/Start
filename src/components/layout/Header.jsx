172efa6b5d9a853e990112a32e46ad1a5e89021f
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-24 20:16:02 +0200

    Add: Header Section

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,46 @@
+import { useState } from 'react';
+import { Link } from 'react-router';
 export function Header() {
-  return <>
-  
-  
-  </>;
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <>
+      <header className=" sticky top-0 bg-[#74c69d] text-white py-5">
+        <div className="container flex justify-between items-center w-[90%] m-auto">
+          <div className="logo">
+            <Link to="/" className="flex items-center text-4xl">
+              <img className=" w-12 mr-2" src="/assets/icons/logo.webp" alt="logo" />
+              start
+            </Link>
+          </div>
+          <nav>
+            <button className="md:hidden" onClick={() => setOpenMenu(!openMenu)}>
+              <img src="/assets/icons/menu.webp" alt="menu icon" />
+            </button>
+            <ul className={`${openMenu ? 'absolute right-0 top-16 w-[200px] rounded-xl bg-[#74c69d] p-3' : 'hidden'} md:flex gap-9 items-center`}>
+              <li>
+                <Link className=" text-[18px] font-semibold" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className=" text-[18px] font-semibold" to="/">
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                <Link className=" text-[18px] font-semibold" to="/">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link className=" text-[18px] font-semibold" to="/">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { Header } from './components/layout/Header.jsx';
+import { BrowserRouter, Routes } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <BrowserRouter>
+      <Header />
+      <Routes></Routes>
+    </BrowserRouter>
   </StrictMode>
 );
