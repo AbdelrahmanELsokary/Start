da45d8a03e090be5731e6796c7d5b0a61605de66
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-24 20:30:22 +0200

    Create: Button File

---
+++ src/components/pages/Hero.jsx
@@ -0,0 +1,21 @@
+export function Hero() {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="col_1">
+            <span>WELCOME</span>
+            <h1>Lorem ipsum dolor sit amet consectetur</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, voluptatibus. ipsum dolor, sit amet consectetur adipisicing elit. Odio harum rem dolor quos impedit ea. Lorem ipsum
+              dolor sit amet consectetur, adipisicing elit. Dolorem, optio?
+            </p>
+          </div>
+          <div className="col_2">
+            <img src="/public/assets/images/hero.webp" alt="hero image" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { Header } from './components/layout/Header.jsx';
 import { BrowserRouter, Routes } from 'react-router';
+import { Hero } from './components/pages/Hero.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <BrowserRouter>
       <Header />
+      <Hero />
       <Routes></Routes>
     </BrowserRouter>
   </StrictMode>
