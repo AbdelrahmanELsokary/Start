cc1623f404423e7c2c8a15ab6f2db9f298f3bf22
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-26 20:59:25 +0200

    Rename: Blog File To Team

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-// import App from './App.jsx';
+import App from './App.jsx';
 import { Header } from './components/layout/Header.jsx';
 import { BrowserRouter, Routes } from 'react-router';
-import { Hero } from './components/pages/home/Hero.jsx';
-import { Partners } from './components/pages/home/prand.jsx';
-import { About } from './components/pages/home/About_Our.jsx';
+// import { Hero } from './components/pages/home/Hero.jsx';
+// import { Partners } from './components/pages/home/prand.jsx';
+// import { About } from './components/pages/home/About_Our.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <BrowserRouter>
       <Header />
-      <Hero />
+      <App />
+      {/* <Hero />
       <Partners />
-      <About />
+      <About /> */}
       <Routes></Routes>
     </BrowserRouter>
   </StrictMode>
