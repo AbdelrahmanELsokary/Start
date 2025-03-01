c0add40c2d90a1bb2c8b6cef2bdc20abe64bd8be
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-03-01 16:05:43 +0200

    Remove: Import Css File Of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Hero } from './components/pages/home/Hero.jsx';
 import { Partners } from './components/pages/home/prand.jsx';
 import { About } from './components/pages/home/About_Our.jsx';
